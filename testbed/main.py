885e1dbe97549af57b8644866367ee676dd4260d
Oskar Tornevall <oskar.tornevall@gmail.com>
2022-02-26 15:52:57 +0100

    Small change to precision test

--- testbed/main.py
+++ testbed/main.py
@@ -23,13 +23,17 @@ TESTS = [
     ("atan", atan, mpmath.mpf(-100), mpmath.mpf(100), 336),
     ("ln", ln, mpmath.mpf(0.1), mpmath.mpf(100), 337),
     ("exp", exp, mpmath.mpf(-100), mpmath.mpf(100), 338),
-    ("inv", inv, mpmath.mpf(1), mpmath.mpf(10), 339)
+    #("inv", inv, mpmath.mpf(1), mpmath.mpf(10), 339)
 ]
 
 def run_tests():
     plt.ioff()
+    w = 2
+    h = 4
+    i = 1
     for t in TESTS:
-        test_precision.test(t[0], t[1], t[4], 10000, t[2], t[3])
+        test_precision.test(t[0], t[1], w * 10 + h * 100 + i, 10000, t[2], t[3])
+        i += 1
     plt.show()
 
 if __name__ == "__main__":
